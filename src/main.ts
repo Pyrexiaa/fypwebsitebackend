ace55a3aaf23771f036b7ebeb334568226c17a94
Jia Yu Lim <lim.jia.yu.desmond@gmail.com>
2024-12-19 02:01:00 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,11 @@ async function bootstrap() {
 
   // Enable CORS with the frontend URL
   app.enableCors({
-    origin: frontendUrl,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: ['https://fypwebsitebackend-drab.vercel.app/', frontendUrl],
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
     credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
   });
 
   await app.listen(3000);
