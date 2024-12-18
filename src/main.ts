0ebaa3def0523d9de59140aba117af7d87ce710d
Jia Yu Lim <jiayu.lim@ailensgroup.com>
2024-12-19 01:50:33 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,23 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
+  // Get FRONTEND_URL from environment variables or default to localhost
+  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
+
+  if (!process.env.FRONTEND_URL) {
+    console.warn(
+      'Warning: FRONTEND_URL is not defined. Falling back to http://localhost:3000',
+    );
+  }
+
+  // Enable CORS with the frontend URL
   app.enableCors({
-    origin: 'http://localhost:3001', // frontend URL
+    origin: frontendUrl,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Allow cookies if needed
+    credentials: true,
   });
 
   await app.listen(3000);
 }
+
 bootstrap();
